ffeca30adf0291014668d5dbd35bfb2ff0c18b52
VINNYSCOSTA <44821757+vinnyscosta@users.noreply.github.com>
2021-04-05 19:05:33 -0300

    Update login.php

--- login.php
+++ login.php
@@ -110,7 +110,6 @@
             <div class="form-group">
                 <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
                 <div class="col-sm-10">
-                <input type="email" class="form-control" id="inputEmail3" name="email" placeholder="Email">
                 </div>
             </div>
             <div class="form-group">
